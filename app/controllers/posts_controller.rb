05a53fa6ecf2b66ecbd52a3cc7732f6427dd719f
sai2231 <seesa.sai222@gmail.com>
2019-10-01 19:52:25 +0530

    Edit and Delete Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
 	end
 	
 	def new
+	 @post = Post.new	
 	end
 
 	def show
@@ -12,10 +13,36 @@ class PostsController < ApplicationController
 
 	def create
 	  @post = Post.new(post_params)
-	  @post.save
 
-	  redirect_to @post
+	   if @post.save
+	   	  redirect_to @post
+	   	else 
+	   	  render 'new'
+	 end	   
+
 	end
+
+	def edit
+	  @post = Post.find(params[:id])
+	end
+
+	def update
+	 @post = Post.find(params[:id])
+
+	 if post.update(params[:post].permit(:title, :body))
+	 	redirect_to @post
+	 else
+	   render 'edit'
+	 end
+	end
+
+	def destroy
+	 @post = Post.find(params[:id])
+	 @post.destroy
+
+	 redirect_to posts_path
+	end   	
+
 
 	private
 	  def post_params
